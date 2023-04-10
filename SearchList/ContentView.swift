c145243086c59d2b060b658ba0d688aacab9ebe2
Russell Gordon <rgordon@lcs.on.ca>
2023-04-10 14:56:56 -0400

    Added the .searchable view modifier.

--- SearchList/ContentView.swift
+++ SearchList/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct ContentView: View {
     
     // MARK: Stored properties
+    
+    // The list of items to show
     @State var items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+    
+    // The search term the user has provided
+    @State var searchText = ""
 
     // MARK: Computed properties
     var body: some View {
@@ -18,6 +23,7 @@ struct ContentView: View {
         List(items, id: \.self) { currentItem in
             Text(currentItem)
         }
+        .searchable(text: $searchText)
         
     }
 }
