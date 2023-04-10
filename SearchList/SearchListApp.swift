1e9c914c1d37d912147c83002bcf9cba499cdf0a
Russell Gordon <rgordon@lcs.on.ca>
2023-04-10 14:26:42 -0400

    Initial Commit

---
+++ SearchList/SearchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchListApp.swift
+//  SearchList
+//
+//  Created by Russell Gordon on 2023-04-10.
+//
+
+import SwiftUI
+
+@main
+struct SearchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
